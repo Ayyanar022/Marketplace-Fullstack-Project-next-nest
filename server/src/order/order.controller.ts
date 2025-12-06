a5f59976e3f2e8f939c6dd9ae4aac763eb7aea60
Ayyanar <ayan.prof022@example.com>
2025-12-06 23:30:22 +0530

    till order

--- server/src/order/order.controller.ts
+++ server/src/order/order.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGaurd } from 'src/auth/guard/jwt-auth.guard';
+import { OrderService } from './order.service';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { RoleGuard } from 'src/auth/guard/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { orderStatus } from '@prisma/client';
 
+@UseGuards(JwtAuthGaurd)
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private orderService :OrderService){}
+
+    @Post()
+    placeOrder(@CurrentUser() user){
+        return this.orderService.placeOrder(user.id);
+    }
+
+    @Get()
+    getUserOrders(@CurrentUser()user){
+        return this.orderService.getUserOrders(user.id)
+    }
+
+    @UseGuards(RoleGuard)
+    @Roles("SELLER",'ADMIN')
+    @Get('seller')
+    getSellerOrder(@CurrentUser() user){
+        return this.orderService.getSellerOredrs(user.id)
+    }
+
+    @UseGuards(RoleGuard)
+    @Roles('SELLER','ADMIN')
+    @Patch(':id/status')
+    updateSattus(@Param('id') orderId:string , @Body('status')status:orderStatus){
+            return this.orderService.updateStatus(orderId,status)
+    }
+}
--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -1,4 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { orderStatus } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(private prisma:PrismaService){}
+
+    //1. Takes user’s cart
+    //  Creates order
+    //  Stores order items
+    //  Clears the cart
+    async placeOrder(userId:string){
+        const cart = await this.prisma.cart.findUnique({
+            where:{id:userId},
+            include:{
+                cartItems:{
+                    include:{product:true}
+                }
+            }
+        })
+        
+
+         if(!cart || cart.cartItems.length===0) throw new  BadRequestException("Cart is Empty");
+
+         // calculate total 
+         const total = cart.cartItems.reduce((sum,item)=>{
+            return  sum+ item.quantity* item.product.price;
+         },0)
+
+
+         // create order
+         const order = await  this.prisma.order.create({
+            data:{
+                userId,
+                total,
+                items:{
+                    create:cart.cartItems.map((item)=>({
+                        productId :item.productId,
+                        quantity:item.quantity,
+                        price:item.product.price
+                    }))
+                },           
+            },
+            include:{items:true}
+         })
+
+         // clear cart
+         await this.prisma.cartItems.deleteMany({
+            where:{cartId:cart.id},
+         })
+
+         
+    }
+
+
+    // get user orders
+    async getUserOrders(userId:string){
+        return this.prisma.order.findMany({
+            where:{userId},
+            include:{
+                items:{
+                    include:{product:true}
+                }
+            },
+            orderBy:{createdAt:'desc'},
+        })
+    }
+
+    //get seller order 
+    async getSellerOredrs(sellerId:string){
+        return this.prisma.order.findMany({
+            where:{
+                items:{
+                    some:{
+                        product:{sellerId}
+                    }
+                }
+            },
+            include:{
+                items:{
+                    include:{product:true}
+                }
+            }
+        })
+    }
+
+    // update status
+    async updateStatus(orderId:string,status:orderStatus){
+        return this.prisma.order.update({
+            where:{id:orderId},
+            data:{status}
+        })
+    }
+
+
+
+
+   
+}
