307074a0d9f0c707dc5cff7bc01c16c0e235a0ee
Ayyanar <ayan.prof022@example.com>
2025-12-13 14:41:28 +0530

    setup and folderstrucuture

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        primaryHover: "var(--color-primary-hover)",
+        textPrimary: "var(--color-text-primary)",
+        textSecondary: "var(--color-text-secondary)",
+        bgMain: "var(--color-bg-main)",
+        cardBg: "var(--color-card-bg)",
+        accent: "var(--color-accent)",
+      },
+    },
   },
   plugins: [],
 };
